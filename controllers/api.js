1b8449019e2956050fb7ffb8303e40489de523b8
John Osterfeld <jczarfeld@gmail.com>
2021-08-17 08:54:17 -0400

    fixed up the duration component of the workout

--- controllers/api.js
+++ controllers/api.js
@@ -1,13 +1,22 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.get("/api/workouts", (req, res) => {
-    Workout.find({}).then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+router.get('/api/workouts', (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    },
+  ])
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.put("/api/workouts/:id", (req, res) => {
@@ -31,13 +40,26 @@ router.post("/api/workouts", (req, res) => {
       .catch(err => res.json(err));
   });
 
-router.get("/api/workouts/range", ({}, res) => {
-    Workout.find({}).then((dbWorkout) => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    });
-});
+  router.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: '$exercises.duration',
+          },
+        },
+      },
+    ])
+      .sort({ _id: -1 })
+      .limit(7)
+      .then((dbWorkouts) => {
+        console.log(dbWorkouts);
+        res.json(dbWorkouts);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
 
 
